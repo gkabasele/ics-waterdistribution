7d0b6524f038a32dcd6e93c28d2e6698a2b911f1
gkabasele <Gnicolas.KN@gmail.com>
2018-04-06 02:49:57 -0700

    create new directory argument

--- examples/simpy-ics/script_plc_tank3.py
+++ examples/simpy-ics/script_plc_tank3.py
@@ -8,7 +8,8 @@ print "Launching tank3"
 
 def main(args):
     plc = PLC(args.ip, args.port, args.store, "plc-tank3", tank3 = (HR,1))
-#    plc.export_variables(args.filename)
+    if args.create_ex:
+        plc.export_variables(args.filename)
     plc.run('plc-tank3', args.period, args.duration)
     plc.wait_end(True)
     
@@ -20,5 +21,6 @@ if __name__ == "__main__":
     parser.add_argument("--period", dest="period", type=int, default=1, action="store")
     parser.add_argument("--duration", dest="duration", type=int, default=60, action="store")
     parser.add_argument("--export", dest="filename", action="store")
+    parser.add_argument("--create", dest="create_ex", action="store_true")
     args = parser.parse_args()
     main(args)
